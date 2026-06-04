5451b3a332a88317d611f2eaf4814155315d9bb5
Rishabh-iitj2029 <rishabhraj456789@gmail.com>
2026-06-04 22:08:37 +0530

    rememeber the walrus operator ':=' is allowed only inside any method not globally

---
+++ 02_variable/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+jwtToken := 6514654981
+
+func main() {
+	var username string = "Rishabh"
+	fmt.Println(username)
+	fmt.Printf("Variable type is : %T\n", username)
+
+	var isVerified bool = false
+	fmt.Println(isVerified)
+	fmt.Printf("Variable type is : %T\n", isVerified)
+
+	var smallValue int = 256
+	fmt.Println("The value is ",smallValue)
+	fmt.Printf("Variable type is : %T\n", smallValue)
+
+	var samllFloat float64 = 255.457878142472147
+	fmt.Println(samllFloat)
+	fmt.Printf("Variable type is : %T\n", samllFloat)
+
+	var another string;
+	fmt.Println(another)
+	fmt.Printf("The type of variable is %T\n", another)
+
+	// Implicit type of declaring the var
+
+	var website = "hello it's my portfolio"
+	fmt.Println(website)
+
+	// no var style
+
+	numberOfUser := 4578
+	fmt.Println(numberOfUser)
+}
