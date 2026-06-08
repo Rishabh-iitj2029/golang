6c55d5bccc34b35691b8939a54cf9c98d5ebe9b3
Rishabh-iitj2029 <rishabhraj456789@gmail.com>
2026-06-08 22:35:12 +0530

    updated the CLI tool

--- CLI_tool/main.go
+++ CLI_tool/main.go
@@ -4,46 +4,76 @@ import (
 	"fmt"
 	"net/http"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
-var counter int
+
+
+type responseType struct {
+	Endpoint   string
+	StatusCode int
+}
 
 func main() {
 
-	fmt.Println("---------------SENDING REQUEST------------------")
-	wg := &sync.WaitGroup{}
-	mut := &sync.Mutex{}
+	for {
+		var counter int64
 
-	urlendpoints := []string{
-		"https://www.google.com/",
-		"https://projects.devluplabs.tech/projects",
-		"https://github.com/Rishabh-iitj2029",
-		"https://leetcode.com/",
-		"https://medium.com/@shikha.ritu17/rest-api-architecture-6f1c3c99f0d3",
-		"https://www.youtube.com/watch?v=i5HEE5Ikv3w&list=PLRAV69dS1uWQGDQoBYMZWKjzuhCaOnBpa&index=56",
-		"https://pkg.go.dev/",
+		fmt.Println("---------------SENDING REQUEST------------------")
+		wg := &sync.WaitGroup{}
+
+		urlendpoints := []string{
+			"https://www.google.com/",
+			"https://projects.devluplabs.tech/projects",
+			"https://github.com/Rishabh-iitj2029",
+			"https://leetcode.com/",
+			"https://medium.com/@shikha.ritu17/rest-api-architecture-6f1c3c99f0d3",
+			"https://www.youtube.com/watch?v=i5HEE5Ikv3w&list=PLRAV69dS1uWQGDQoBYMZWKjzuhCaOnBpa&index=56",
+			"https://pkg.go.dev/",
+		}
+
+		client := &http.Client{
+			Timeout: 10 * time.Second,
+		}
+
+		ch := make(chan responseType, len(urlendpoints))
+
+		for _, url := range urlendpoints {
+			wg.Add(1)
+			go func(target string) {
+				defer wg.Done()
+				res, err := client.Get(target)
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				if res.StatusCode == 200 {
+					atomic.AddInt64(&counter, 1)
+				}
+
+				ch <- responseType{target, res.StatusCode}
+				res.Body.Close()
+
+			}(url)
+		}
+
+		results := make([]responseType,0,len(urlendpoints))
+
+		go func() {
+			wg.Wait()
+			close(ch)
+		}()
+
+		for url := range ch {
+			results = append(results, url)
+		}
+
+		for _, val := range results {
+			fmt.Printf("%v\n", val)
+		}
+		fmt.Println("The counter value = ", counter)
+		fmt.Printf("\n")
+		time.Sleep(2 * time.Second)
 	}
-
-	wg.Add(len(urlendpoints))
-	for ind, url := range urlendpoints {
-		go func(wg *sync.WaitGroup, mut *sync.Mutex) {
-			res, err := http.Get(url)
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			defer wg.Done()
-
-			if res.StatusCode == 200 {
-				mut.Lock()
-				counter++
-				mut.Unlock()
-			}
-			fmt.Printf("%v index -- url %v -- status code %v\n", ind+1, url, res.StatusCode)
-
-		}(wg,mut)
-	}
-	wg.Wait()
-
-	fmt.Println("The counter value = ", counter)
 }
