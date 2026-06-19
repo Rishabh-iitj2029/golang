c993cc1c7907bcf52f14328e977bc51f2eaff6ec
Rishabh-iitj2029 <rishabhraj456789@gmail.com>
2026-06-19 17:07:44 +0530

    added the tools where agent calls a server and request a service and server verifies whether the agent is registered then sends back the requested data

---
+++ Using_langchain/main.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI,APIRouter
+from typing import List
+from fastapi  import Request
+
+app = FastAPI()
+
+router = APIRouter(prefix="/api/v1")
+app.include_router(router)
+# making a fake database
+
+class Agent:
+    def __init__(self,agentId:str,payment_status:str, validity:int):
+        self.agentId = agentId
+        self.payment_status = payment_status
+        self.validity = validity
+
+agent_database:List[Agent] = []
+agent_database.append(Agent("4578","done",45))
+
+
+@router.get("/research/{agentId}")
+async def handle_research(agentId:str, request:Request):
+    isRegistered = False
+
+    for agent in agent_database:
+        if(agent.agentId == agentId):
+            isRegistered = True
+            break
+
+    if not isRegistered:
+        return{
+            "message":"Payment required, you are not registered in my server",
+            "ToPay":"700Rs"
+        }
+    
+    payload = await request.body()
+    print(payload)
+    return {
+        "message": "Access granted",
+        "data":"This is you requested data => -------------------- "
+    }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+
+
+
+    
+    
+
+    
--- Using_langchain/tool.py
+++ Using_langchain/tool.py
@@ -2,6 +2,7 @@ from langchain_groq import ChatGroq
 from langchain_core.tools import tool
 import os
 import httpx
+import requests
 import config
 from langchain_core.messages import HumanMessage, BaseMessage,ToolMessage
 
@@ -12,14 +13,23 @@ def exchange_rate(base:str, quotes:str):
     res = res.json()
     return f"As per the date {res[0]['date']}\n1 {base} = {res[0]['rate']} {quotes}"
 
+
+@tool(description="for making reasearch")
+def make_research(agentId:str,msg:str):
+    apiUrl = f"http://127.0.0.1:8000/api/v1/research/{agentId}"
+    body_data = {"service":msg}
+
+    res = requests.get(apiUrl,json=body_data)
+    return res.json()
+    
+
 llm = ChatGroq(model="llama-3.3-70b-versatile", temperature=0)
 
-tools = [exchange_rate]
+tools = [exchange_rate, make_research]
 llm_with_tools = llm.bind_tools(tools)
 
 def run_agent(user_prompt:str):
     msgs:list[BaseMessage] = [HumanMessage(content=user_prompt)]
-    messages = [HumanMessage(content=user_prompt)]
     tool_mapping = {tool.name: tool for tool in tools}
 
     res = llm_with_tools.invoke(msgs)
@@ -29,13 +39,13 @@ def run_agent(user_prompt:str):
         tool_name = tool_call["name"]
         tool_args = tool_call["args"]
         tool_id = tool_call["id"]
-        print(f"[Thinking...] Llama requested tool: '{tool_name}' with arguments: {tool_args}")
+        print(f"Llama requested tool: '{tool_name}' with arguments: {tool_args}")
 
         target_tool = tool_mapping[tool_name]
         tool_output = target_tool.invoke(tool_args)
 
             
-        print(f"[Observation] Tool returned: {tool_output}")
+        print(f"Tool returned: {tool_output}")
 
         msgs.append(
                 ToolMessage(
@@ -45,3 +55,5 @@ def run_agent(user_prompt:str):
             )
 
 run_agent("what is current exchange rate between USD and EUR")
+print("\n-----------------------------------------------------------------------------------------------------------------------------------")
+run_agent("research about the blockchain network, my agent id is 4578")
